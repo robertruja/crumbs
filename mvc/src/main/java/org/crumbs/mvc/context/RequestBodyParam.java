33556fa317edc50a717ab4a24d13ed5333fde866
Robert Ruja <ruja.robert@gmail.com>
2022-01-04 18:50:37 +0200

    throw bad request on invalid request body

--- mvc/src/main/java/org/crumbs/mvc/context/RequestBodyParam.java
+++ mvc/src/main/java/org/crumbs/mvc/context/RequestBodyParam.java
@@ -2,7 +2,7 @@ package org.crumbs.mvc.context;
 
 import org.crumbs.json.JsonMapper;
 import org.crumbs.json.exception.JsonUnmarshalException;
-import org.crumbs.mvc.exception.HandlerInvocationException;
+import org.crumbs.mvc.exception.BadRequestException;
 import org.crumbs.mvc.http.Request;
 
 public class RequestBodyParam implements HandlerParam {
@@ -17,7 +17,7 @@ public class RequestBodyParam implements HandlerParam {
         try {
             return new JsonMapper().unmarshal(request.getBody(), type);
         } catch (JsonUnmarshalException e) {
-            throw new HandlerInvocationException("Could not deserialize request body of type " + type.getName(), e);
+            throw new BadRequestException("Could not deserialize request body of type " + type.getName(), e);
         }
     }
 }
