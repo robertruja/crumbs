dd515c20b49419bdc22752a879f6a7b0ea2db95d
Robert Ruja <ruja.robert@gmail.com>
2022-03-26 15:53:42 +0200

    Added gzip automatic decompression by encoding response header

--- client/src/main/java/org/crumbs/client/http/impl/sun/HttpClientImpl.java
+++ client/src/main/java/org/crumbs/client/http/impl/sun/HttpClientImpl.java
@@ -8,11 +8,13 @@ import org.crumbs.client.http.model.Response;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.GZIPInputStream;
 
 public class HttpClientImpl implements HttpClient {
 
@@ -43,13 +45,26 @@ public class HttpClientImpl implements HttpClient {
             ResponseImpl response = new ResponseImpl();
             response.setStatusCode(HttpStatus.fromIntCode(httpResponse.statusCode()));
             response.setHeaders(httpResponse.headers().map());
-            response.setPayload(httpResponse.body());
+            response.setPayload(determinePaylodByEncoding(httpResponse));
 
             return response;
         } catch (InterruptedException | IOException | URISyntaxException e) {
             throw new ConnectException("An error occurred when sending request to: " + request.getUrlPath(), e);
         }
     }
+
+    private InputStream determinePaylodByEncoding(HttpResponse<InputStream> response) throws IOException {
+        String encoding = response.headers().firstValue("Content-Encoding").orElse("");
+        switch (encoding) {
+            case "gzip" :
+                return new GZIPInputStream(response.body());
+            case "":
+                return response.body();
+            default:
+                throw new UnsupportedEncodingException("Unsupported encoding: " + encoding);
+        }
+    }
+
 
     private void setHeaders(HttpRequest.Builder builder, Map<String, List<String>> headers) {
         for(Map.Entry<String, List<String>> entry: headers.entrySet()) {
